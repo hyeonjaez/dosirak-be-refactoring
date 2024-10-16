97f02d3fb2515c72eea5604c78fdac958d8fdccc
hyeonjaez <jhjh0022@naver.com>
2024-10-17 00:34:56 +0900

    feat: ChatRoom Register request dto

---
+++ src/main/java/com/example/dosirakbe/domain/chat_room/dto/request/ChatRoomRegisterRequest.java
@@ -0,0 +1,14 @@
+package com.example.dosirakbe.domain.chat_room.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ChatRoomRegisterRequest {
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String title;
+}
