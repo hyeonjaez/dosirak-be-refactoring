a69f4f86c5e6ceab4a956d2820a40ff2b091cc91
Yujin Lee <leeyujin1231@duksung.ac.kr>
2024-10-20 17:26:53 +0900

    fix : JwtFilter 수정 후 시큐리티가 토큰 정보 확인 가능하도록 변경

--- src/main/java/com/example/dosirakbe/domain/user/generator/NickNameGenerator.java
+++ src/main/java/com/example/dosirakbe/domain/user/generator/NickNameGenerator.java
@@ -10,7 +10,8 @@ import java.util.List;
 @Getter
 public class NickNameGenerator {
 
-    private String nickname;
+    private String nickName;
+
     List<String> adjective = Arrays.asList("용기있는", "노력하는", "친환경적인", "에너지를절약하는", "지속가능한");
     List<String> object = Arrays.asList("북극곰" ,"바다물범", "사자", "판다", "호랑이", "표범", "강아지", "고양이");
     public NickNameGenerator(){
@@ -22,7 +23,7 @@ public class NickNameGenerator {
 
         String adj = adjective.get(0);
         String OName = object.get(0);
-        this.nickname = adj+OName+number;
+        this.nickName = adj+OName+number;
 
     }
 
