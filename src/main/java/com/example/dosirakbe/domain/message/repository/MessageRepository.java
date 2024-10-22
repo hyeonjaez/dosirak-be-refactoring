4e88a3e152577d6eee2a7af0a7ee1e0105f6672a
hyeonjaez <jhjh0022@naver.com>
2024-10-23 02:11:05 +0900

    feat : 채팅방 마지막 메세지 가져오는 쿼리문

--- src/main/java/com/example/dosirakbe/domain/message/repository/MessageRepository.java
+++ src/main/java/com/example/dosirakbe/domain/message/repository/MessageRepository.java
@@ -1,11 +1,15 @@
 package com.example.dosirakbe.domain.message.repository;
 
 import com.example.dosirakbe.domain.message.entity.Message;
+import com.example.dosirakbe.domain.message.entity.MessageType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findByChatRoomIdAndCreatedAtAfterOrderByCreatedAtAsc(Long chatRoomId, LocalDateTime createdAt);
+
+    Optional<Message> findFirstByChatRoomIdAndMessageTypeOrderByCreatedAtDesc(Long chatRoomId, MessageType messageType);
 }
