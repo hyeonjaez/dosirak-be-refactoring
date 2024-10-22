232614460b2d746c107f3db5addc2974ecd0809a
hyeonjaez <jhjh0022@naver.com>
2024-10-23 02:11:55 +0900

    feat : 지역카테고리, user가 속하지 않은 채팅방 목록

--- src/main/java/com/example/dosirakbe/domain/chat_room/repository/ChatRoomRepository.java
+++ src/main/java/com/example/dosirakbe/domain/chat_room/repository/ChatRoomRepository.java
@@ -1,7 +1,20 @@
 package com.example.dosirakbe.domain.chat_room.repository;
 
 import com.example.dosirakbe.domain.chat_room.entity.ChatRoom;
+import com.example.dosirakbe.domain.user.entity.User;
+import com.example.dosirakbe.domain.zone_category.entity.ZoneCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    List<ChatRoom> findAllByZoneCategory(ZoneCategory zoneCategory);
+
+    @Query("SELECT cr FROM ChatRoom cr " +
+            "LEFT JOIN UserChatRoom ucr ON cr.id = ucr.chatRoom.id AND ucr.user = :user " +
+            "WHERE cr.zoneCategory = :zoneCategory AND ucr.id IS NULL")
+    List<ChatRoom> findChatRoomsByZoneCategoryAndNotJoinedByUser(@Param("zoneCategory") ZoneCategory zoneCategory,
+                                                                 @Param("user") User user);
 }
