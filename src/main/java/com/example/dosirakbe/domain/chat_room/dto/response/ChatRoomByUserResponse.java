428baa66992c51c3fdc18f7459ecaffed7e2d804
hyeonjaez <jhjh0022@naver.com>
2024-10-23 02:08:15 +0900

    feat : 유저가 속한 채팅방 response dto

---
+++ src/main/java/com/example/dosirakbe/domain/chat_room/dto/response/ChatRoomByUserResponse.java
@@ -0,0 +1,15 @@
+package com.example.dosirakbe.domain.chat_room.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ChatRoomByUserResponse {
+    private Long id;
+    private String title;
+    private Long personCount;
+    private String lastMessage;
+}
