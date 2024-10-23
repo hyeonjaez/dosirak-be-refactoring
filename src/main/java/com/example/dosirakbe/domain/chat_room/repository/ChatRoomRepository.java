b29928a2ec0c0168aacf3553a9d5a602989b8cb4
hyeonjaez <jhjh0022@naver.com>
2024-10-23 11:32:29 +0900

    feat : 제목 검색, 정렬으로 ChatRoom list 가져오는 쿼리

--- src/main/java/com/example/dosirakbe/domain/chat_room/repository/ChatRoomRepository.java
+++ src/main/java/com/example/dosirakbe/domain/chat_room/repository/ChatRoomRepository.java
@@ -3,6 +3,7 @@ package com.example.dosirakbe.domain.chat_room.repository;
 import com.example.dosirakbe.domain.chat_room.entity.ChatRoom;
 import com.example.dosirakbe.domain.user.entity.User;
 import com.example.dosirakbe.domain.zone_category.entity.ZoneCategory;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,4 +18,8 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
             "WHERE cr.zoneCategory = :zoneCategory AND ucr.id IS NULL")
     List<ChatRoom> findChatRoomsByZoneCategoryAndNotJoinedByUser(@Param("zoneCategory") ZoneCategory zoneCategory,
                                                                  @Param("user") User user);
+
+    List<ChatRoom> findByTitleContainingIgnoreCase(String title, Sort sort);
+
+    List<ChatRoom> findAll(Sort sort);
 }
