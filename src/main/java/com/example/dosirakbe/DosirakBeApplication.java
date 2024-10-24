5b4d9c990e1f0cf3316047e8f9eb1f76113a5826
Yujin Lee <leeyujin1231@duksung.ac.kr>
2024-10-25 04:11:06 +0900

    chore : 메인 브렌치 머지

--- src/main/java/com/example/dosirakbe/DosirakBeApplication.java
+++ src/main/java/com/example/dosirakbe/DosirakBeApplication.java
@@ -4,15 +4,15 @@ import com.example.dosirakbe.global.util.ApiResult;
 import com.example.dosirakbe.global.util.StatusEnum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class DosirakBeApplication {
 
     public static void main(String[] args) {
-        System.out.println();
-
 
 
         SpringApplication.run(DosirakBeApplication.class, args);
--- src/main/java/com/example/dosirakbe/domain/user/dto/response/UserDTO.java
+++ src/main/java/com/example/dosirakbe/domain/user/dto/response/UserDTO.java
@@ -7,7 +7,6 @@ import lombok.Setter;
 @Setter
 public class UserDTO {
 
-
     private Long userId;
     private String userName;
     private String name;
