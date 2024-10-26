75598ff75b2616e45ecef5e6d1377c1c6ac5f472
femmefatalehaein <rmfls4359@gmail.com>
2024-10-26 18:05:57 +0900

    feat : add lib ...

--- src/main/java/com/example/dosirakbe/global/util/ExceptionEnum.java
+++ src/main/java/com/example/dosirakbe/global/util/ExceptionEnum.java
@@ -15,7 +15,13 @@ public enum ExceptionEnum {
     // Custom Exception
     SECURITY(HttpStatus.UNAUTHORIZED, "CE0001", "로그인이 필요합니다"),
     DATA_NOT_FOUND(HttpStatus.NOT_FOUND, "CE0002", "데이터를 찾을 수 없습니다"),
-    PERMISSION_DENIED(HttpStatus.FORBIDDEN, "CE0003", "권한이 없습니다");
+    PERMISSION_DENIED(HttpStatus.FORBIDDEN, "CE0003", "권한이 없습니다"),
+    DUPLICATE_IMAGE(HttpStatus.BAD_REQUEST, "CE0004", "중복된 이미지가 업로드되었습니다."),
+    NO_IMAGE_EXIST(HttpStatus.NOT_FOUND, "CE0005", "이미지가 존재하지 않습니다."),
+    FAIL_DELETE(HttpStatus.INTERNAL_SERVER_ERROR, "CE0006", "파일 삭제에 실패했습니다."),
+    FAIL_UPLOAD(HttpStatus.INTERNAL_SERVER_ERROR, "CE0007", "파일 업로드에 실패했습니다."),
+    NOT_IMAGE_EXTENSION(HttpStatus.BAD_REQUEST, "CE0008", "허용되지 않는 파일 확장자입니다.");
+
 
     private final HttpStatus status;
     private final String code;
