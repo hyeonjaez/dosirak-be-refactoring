5e3df81047aa17d56e691be345e7961afad65dab
femmefatalehaein <rmfls4359@gmail.com>
2024-10-27 16:28:40 +0900

    feat : jasypt 암호화

--- src/main/java/com/example/dosirakbe/DosirakBeApplication.java
+++ src/main/java/com/example/dosirakbe/DosirakBeApplication.java
@@ -2,10 +2,12 @@ package com.example.dosirakbe;
 
 import com.example.dosirakbe.global.util.ApiResult;
 import com.example.dosirakbe.global.util.StatusEnum;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableEncryptableProperties
 @EnableJpaAuditing
 @SpringBootApplication
 public class DosirakBeApplication {
