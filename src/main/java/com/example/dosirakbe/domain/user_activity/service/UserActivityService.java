150df85e253f03748aed947da711fdea76ef5dd5
hyeonjaez <jhjh0022@naver.com>
2024-11-03 17:43:13 +0900

    feat : 해당 월의 유저 활동 리스트 조회 service

--- src/main/java/com/example/dosirakbe/domain/user_activity/service/UserActivityService.java
+++ src/main/java/com/example/dosirakbe/domain/user_activity/service/UserActivityService.java
@@ -1,29 +1,36 @@
 package com.example.dosirakbe.domain.user_activity.service;
 
+
 import com.example.dosirakbe.domain.user.entity.User;
 import com.example.dosirakbe.domain.user.repository.UserRepository;
 import com.example.dosirakbe.domain.user_activity.dto.mapper.UserActivityMapper;
 import com.example.dosirakbe.domain.user_activity.dto.response.UserActivityResponse;
 import com.example.dosirakbe.domain.user_activity.entity.UserActivity;
 import com.example.dosirakbe.domain.user_activity.repository.UserActivityRepository;
+import com.example.dosirakbe.global.util.ApiException;
+import com.example.dosirakbe.global.util.ExceptionEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.YearMonth;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserActivityService {
     private final UserActivityRepository userActivityRepository;
     private final UserRepository userRepository;
     private final UserActivityMapper userActivityMapper;
 
-    public List<UserActivityResponse> getUserActivityList(Long userId, YearMonth yearMonth) {
-        User user = userRepository.findById(userId).orElseThrow(); //TODO throw
-        LocalDate startDate = yearMonth.atDay(1);
-        LocalDate endDate = yearMonth.atEndOfMonth();
+    public List<UserActivityResponse> getUserActivityList(Long userId, LocalDate month) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(
+                        () -> new ApiException(ExceptionEnum.DATA_NOT_FOUND));
+
+        LocalDate startDate = month.withDayOfMonth(1);
+        LocalDate endDate = month.withDayOfMonth(month.lengthOfMonth());
 
         List<UserActivity> byUserAndCreatedAtBetweenOrderByCreatedAtAsc = userActivityRepository.findByUserAndCreatedAtBetweenOrderByCreatedAtAsc(user, startDate, endDate);
 
