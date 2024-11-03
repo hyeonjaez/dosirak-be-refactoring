9f49c0319f58a7c122e17f9b0983a9ef3fd53faa
hyeonjaez <jhjh0022@naver.com>
2024-11-03 21:37:08 +0900

    feat : 00시 00분 으로 시간 string format 필드 값 추가로 인한 mapper 에 추가 설정

--- src/main/java/com/example/dosirakbe/domain/activity_log/dto/mapper/ActivityLogMapper.java
+++ src/main/java/com/example/dosirakbe/domain/activity_log/dto/mapper/ActivityLogMapper.java
@@ -4,14 +4,25 @@ import com.example.dosirakbe.domain.activity_log.dto.response.ActivityLogRespons
 import com.example.dosirakbe.domain.activity_log.entity.ActivityLog;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface ActivityLogMapper {
-    @Mapping(source = "activityType.type.message", target = "activityMessage")
+
+    @Mapping(source = "activityType.message", target = "activityMessage")
+    @Mapping(source = "createdAt", target = "createAtTime", qualifiedByName = "formatDateTime")
     ActivityLogResponse mapToActivityLogResponse(ActivityLog activityLog);
 
     List<ActivityLogResponse> mapToActivityLogResponseList(List<ActivityLog> activityLogs);
+
+    @Named("formatDateTime")
+    default String formatDateTime(LocalDateTime createdAt) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH시 mm분");
+        return createdAt.format(formatter);
+    }
 }
