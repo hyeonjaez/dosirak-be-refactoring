fdc6e651dfe7206ba5dcaa466bdae6407dfe5278
hyeonjaez <jhjh0022@naver.com>
2024-11-14 23:38:23 +0900

    feat : 이동 거리 추가 api

--- src/main/java/com/example/dosirakbe/domain/track/controller/TrackController.java
+++ src/main/java/com/example/dosirakbe/domain/track/controller/TrackController.java
@@ -1,10 +1,18 @@
 package com.example.dosirakbe.domain.track.controller;
 
+import com.example.dosirakbe.domain.auth.dto.response.CustomOAuth2User;
 import com.example.dosirakbe.domain.track.dto.request.TrackMoveRequest;
+import com.example.dosirakbe.domain.track.dto.response.TrackMoveResponse;
+import com.example.dosirakbe.domain.track.service.TrackService;
 import com.example.dosirakbe.global.util.ApiException;
+import com.example.dosirakbe.global.util.ApiResult;
 import com.example.dosirakbe.global.util.ExceptionEnum;
+import com.example.dosirakbe.global.util.StatusEnum;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,17 +24,32 @@ import java.math.BigDecimal;
 @RestController
 @RequestMapping("/api/track")
 public class TrackController {
+    public final TrackService trackService;
     public static final BigDecimal GAP_DISTANCE = BigDecimal.ONE; //1km
 
     @PostMapping
-    public void recordMovingDistance(@RequestBody @Valid TrackMoveRequest trackMoveRequest) {
+    public ResponseEntity<ApiResult<TrackMoveResponse>> recordMovingDistance(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+                                                                             @RequestBody @Valid TrackMoveRequest trackMoveRequest) {
+        Long userId = customOAuth2User.getUserDTO().getUserId();
+
         if (!checkGapDistance(trackMoveRequest)) {
             throw new ApiException(ExceptionEnum.INVALID_REQUEST);
         }
+
+        TrackMoveResponse trackMoveResponse = trackService.recordTrackDistance(userId, trackMoveRequest);
+        ApiResult<TrackMoveResponse> result = ApiResult.<TrackMoveResponse>builder()
+                .status(StatusEnum.SUCCESS)
+                .message("track move distance successful")
+                .data(trackMoveResponse)
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(result);
     }
 
 
-    public boolean checkGapDistance(TrackMoveRequest trackMoveRequest) {
+    private boolean checkGapDistance(TrackMoveRequest trackMoveRequest) {
         BigDecimal shortestDistance = trackMoveRequest.getShortestDistance();
         BigDecimal moveDistance = trackMoveRequest.getMoveDistance();
 
