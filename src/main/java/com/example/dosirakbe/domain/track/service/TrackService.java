e629e2d2267b8a6e81adc4284b47898a0a8b7a36
hyeonjaez <jhjh0022@naver.com>
2024-11-14 23:38:18 +0900

    feat : 이동 거리 추가

--- src/main/java/com/example/dosirakbe/domain/track/service/TrackService.java
+++ src/main/java/com/example/dosirakbe/domain/track/service/TrackService.java
@@ -1,26 +1,30 @@
 package com.example.dosirakbe.domain.track.service;
 
-import com.example.dosirakbe.domain.activity_log.repository.ActivityLogRepository;
+import com.example.dosirakbe.domain.activity_log.entity.ActivityType;
+import com.example.dosirakbe.domain.green_commit.event.GreenCommitEvent;
 import com.example.dosirakbe.domain.track.dto.request.TrackMoveRequest;
+import com.example.dosirakbe.domain.track.dto.response.TrackMoveResponse;
 import com.example.dosirakbe.domain.user.entity.User;
 import com.example.dosirakbe.domain.user.repository.UserRepository;
-import com.example.dosirakbe.domain.user_activity.repository.UserActivityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.sound.midi.Track;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
 public class TrackService {
     private final UserRepository userRepository;
-    private final UserActivityRepository userActivityRepository;
-    private final ActivityLogRepository activityLogRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
-    public void recordTrackDistance(Long userId, TrackMoveRequest trackMoveRequest) {
+    public TrackMoveResponse recordTrackDistance(Long userId, TrackMoveRequest trackMoveRequest) {
         User user = userRepository.findById(userId).orElseThrow();
 
+        user.setTrackDistance(trackMoveRequest.getMoveDistance());
+        eventPublisher.publishEvent(new GreenCommitEvent(this, user.getUserId(), null, ActivityType.LOW_CARBON_MEANS_OF_TRANSPORTATION, trackMoveRequest.getMoveDistance()));
+
+        return new TrackMoveResponse(trackMoveRequest.getMoveDistance());
     }
 }
