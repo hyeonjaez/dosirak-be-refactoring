7111e7dca5ad5d3a0ce6f0129ad0301788900c0f
Yujin Lee <leeyujin1231@duksung.ac.kr>
2024-11-15 15:33:47 +0900

    feat : greenheroes controller, dto, service 구현

--- src/main/java/com/example/dosirakbe/DosirakBeApplication.java
+++ src/main/java/com/example/dosirakbe/DosirakBeApplication.java
@@ -3,10 +3,14 @@ package com.example.dosirakbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableJpaAuditing
+@EnableScheduling
+@EnableCaching
 @SpringBootApplication
 public class DosirakBeApplication {
 
