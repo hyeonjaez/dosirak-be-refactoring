f5556cee8c703610139899523012b12688a44acf
hyeonjaez <jhjh0022@naver.com>
2024-11-15 13:52:11 +0900

    refactor : api 데이터 받는 방식 수정으로 인한 수정

--- src/main/java/com/example/dosirakbe/domain/seoul_bike_info/service/SeoulBikeInfoService.java
+++ src/main/java/com/example/dosirakbe/domain/seoul_bike_info/service/SeoulBikeInfoService.java
@@ -1,7 +1,6 @@
 package com.example.dosirakbe.domain.seoul_bike_info.service;
 
 import com.example.dosirakbe.domain.seoul_bike_info.dto.mapper.SeoulBikeInfoMapper;
-import com.example.dosirakbe.domain.seoul_bike_info.dto.request.SeoulBikeInfoRequest;
 import com.example.dosirakbe.domain.seoul_bike_info.dto.response.SeoulBikeInfoResponse;
 import com.example.dosirakbe.domain.seoul_bike_info.entity.SeoulBikeInfo;
 import com.example.dosirakbe.domain.seoul_bike_info.repository.SeoulBikeInfoRepository;
@@ -9,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -18,8 +18,8 @@ public class SeoulBikeInfoService {
     private final SeoulBikeInfoRepository seoulBikeInfoRepository;
     private final SeoulBikeInfoMapper seoulBikeInfoMapper;
 
-    public List<SeoulBikeInfoResponse> getSeoulBikeListAroundMe(SeoulBikeInfoRequest seoulBikeInfoRequest) {
-        List<SeoulBikeInfo> allWithinDistance = seoulBikeInfoRepository.findAllWithinDistance(seoulBikeInfoRequest.getMyLatitude(), seoulBikeInfoRequest.getMyLongitude());
+    public List<SeoulBikeInfoResponse> getSeoulBikeListAroundMe(BigDecimal myLatitude, BigDecimal myLongitude) {
+        List<SeoulBikeInfo> allWithinDistance = seoulBikeInfoRepository.findAllWithinDistance(myLatitude, myLongitude);
 
         return seoulBikeInfoMapper.mapToSeoulBikeInfoResponseList(allWithinDistance);
     }
