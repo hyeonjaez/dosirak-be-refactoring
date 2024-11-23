991a06a5dc7e2f614dfafede482e7a154608d40f
hyeonjaez <jhjh0022@naver.com>
2024-11-24 02:08:25 +0900

    refactor : track 기록 하는 entity mapping

---
+++ src/main/java/com/example/dosirakbe/domain/track/entity/Track.java
@@ -0,0 +1,50 @@
+package com.example.dosirakbe.domain.track.entity;
+
+import com.example.dosirakbe.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "track_record")
+@EntityListeners(AuditingEntityListener.class)
+public class Track {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "store_name")
+    private String storeName;
+
+    @Column(name = "store_address")
+    private String storeAddress;
+
+    @Column(name = "distance")
+    private BigDecimal distance;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Track(String storeName, String storeAddress, BigDecimal distance, User user) {
+        this.storeName = storeName;
+        this.storeAddress = storeAddress;
+        this.distance = distance;
+        this.user = user;
+    }
+}
