6b8d017f06387b4e23babc61c39d7e5f62994de6
hyeonjaez <jhjh0022@naver.com>
2024-12-23 00:22:44 +0900

    refactor : TrackMoveResponse dto json snake case @JsonProperty 추가

--- src/main/java/com/example/dosirakbe/domain/track/dto/response/TrackMoveResponse.java
+++ src/main/java/com/example/dosirakbe/domain/track/dto/response/TrackMoveResponse.java
@@ -1,5 +1,6 @@
 package com.example.dosirakbe.domain.track.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -15,6 +16,7 @@ import java.math.BigDecimal;
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
  * 11/14/24        Fiat_lux                최초 생성<br>
+ * 12/23/24        Fiat_lux               필드에 json 어노테이션 추가<br>
  */
 @Getter
 @AllArgsConstructor
@@ -26,5 +28,6 @@ public class TrackMoveResponse {
      * 이 필드는 사용자가 실제로 이동한 트랙 거리를 나타내며, 소수점 이하 두 자리까지의 정밀도를 가집니다.
      * </p>
      */
+    @JsonProperty("move_track_distance")
     private BigDecimal moveTrackDistance;
 }
